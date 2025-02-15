f0956b210843746484c524e6776edd69af179a94
Thomas Kleist <thomasfkleist@gmail.com>
2025-02-15 13:29:06 -0800

    Implement the lower section of the scoreboard

--- yahtzee.cpp
+++ yahtzee.cpp
@@ -14,6 +14,7 @@
 #include <fstream>   //File IO Library
 #include <cmath>     //Math library (need sqrt for standard deviation)
 #include <string>    //string library
+#include <vector>
 using namespace std;
 
 //User Libraries
@@ -22,17 +23,21 @@ using namespace std;
 //Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
 
 // Function Prototypes
-void roll(int [], int);             // Role dice with no hold
-void roll(int [], char[], int);     // Role dice with hold
-void dispDce(int [], int);          // Display dice
-void getHold(char[], int);          // Ask the player if they would like to hold each dice
-int turnScore(int [], int, int);    // Get the score for the turn
-bool gameDone(bool [], int);        // Get if the game is completed based on the row completion
-void displayScore(int [], bool [], int);     // Display the current scores
-int selScore(bool [], int);         // Select which row to score
-void displayTurn();                 // Display the turn number
-void selSort(int [],int);           // Selection sort
-void bubSort(int [],int);           // Selection sort
+void roll(int [], int);                         // Role dice with no hold
+void roll(int [], char[], int);                 // Role dice with hold
+void dispDce(int [], int);                      // Display dice
+void getHold(char[], int);                      // Ask the player if they would like to hold each dice
+bool gameDone(bool [], int, bool [], int);      // Get if the game is completed based on the row completion
+void displayScore(int [], bool [], int, int [], bool [], int);        // Display the current scores
+void selScore(bool [], bool [], int&);              // Select which row to score
+void displayTurn();                             // Display the turn number
+void selSort(int [],int);                       // Selection sort
+void bubSort(int [],int);                       // Selection sort
+void updateScore(int [], bool [], int[], bool [], int, int); // Update the scores based on a turn score
+
+int turnScore(int [], int, int);                // Get the score for the turn
+vector<int> getDiceVals(int [], int);           // Get the number of each value of dice
+int linearSearch(vector<int>, int);
 
 int main(int argc, char** argv) {
     // Set Random Number seed
@@ -43,15 +48,20 @@ int main(int argc, char** argv) {
     const string fileNme = "avgData.dat";
     const int numDice = 5;
     const int numUppR = 6;
+    const int numLwrR = 7;
 
     //Declare Variables
     int dice[numDice]; // The value of the dice
     char hold[numDice]; // Wether or not the user wants to hold each dice. 'Y' or 'y' for yes, anything else for no
     int numSel; // The number the user selects to score this turn
     int turnScr; // The score for this turn
-    int turnCnt; // The counter for how many turns the player had done
+
     int uppScr[numUppR]; // The score for each of numbers on a die
     bool uppDone[numUppR]; // Bool to store if a certain number has been scored
+
+    int lwrScr[numLwrR]; // The score for each of numbers on a die
+    bool lwrDone[numLwrR]; // Bool to store if a certain number has been scored
+    
     int uppSum, bonus, uppTtl; // The sum of all the scores, the bonus points, and the total score for the game
 
     int count; // The count that will be read from the average data file
@@ -65,14 +75,17 @@ int main(int argc, char** argv) {
         uppScr[i] = 0;
         uppDone[i] = false;
     }
-    turnCnt = 1; // Set it to the first turn
+    for (int i = 0; i < numLwrR; i++) {
+        lwrScr[i] = 0;
+        lwrDone[i] = false;
+    }
 
     // Run the game
     // Run the loop until all numbers have been scored
-    while (!gameDone(uppDone, numUppR)) {
+    while (!gameDone(uppDone, numUppR, lwrDone, numLwrR)) {
         // Display turn count and current scores
         displayTurn();
-        displayScore(uppScr, uppDone, numUppR);
+        displayScore(uppScr, uppDone, numUppR, lwrScr, lwrDone, numLwrR);
 
         // Loop 3 times because each turn in yahtzee allows three roles
         for (int i = 0; i < 3; i++) {
@@ -81,7 +94,7 @@ int main(int argc, char** argv) {
             if (i == 0) roll(dice, numDice);
             else roll(dice, hold, numDice);
 
-            if (i == 0) selSort(dice, numDice);
+            if (i == 0 || i == 2) selSort(dice, numDice);
             if (i == 1) bubSort(dice, numDice);
 
             // Display the value of each of the dice
@@ -97,47 +110,9 @@ int main(int argc, char** argv) {
         // After rolling three times, select which number the user would like to score in
         // If the number selected is out of range, ask them to input again
         // If the number they selected has already been scored, ask them to input again
-        numSel = selScore(uppDone, numUppR);
+        selScore(uppDone, lwrDone, numSel);
         turnScr = turnScore(dice, numDice, numSel);
-
-        // Output the number of points earned for the user to see
-        // Save the number of points in the variable for the row
-        // Set the bool of that row to true to indicate that the row has been scored
-        switch (numSel)
-        {
-        case 1:
-            cout << "The number of points earned in the ACE row is " << turnScr << endl << endl << endl;
-            uppScr[0] = turnScr;
-            uppDone[0] = true;
-            break;
-        case 2:
-            cout << "The number of points earned in the TWOS row is " << turnScr << endl << endl << endl;
-            uppScr[1] = turnScr;
-            uppDone[1] = true;
-            break;
-        case 3:
-            cout << "The number of points earned in the THREES row is " << turnScr << endl << endl << endl;
-            uppScr[2] = turnScr;
-            uppDone[2] = true;
-            break;
-        case 4:
-            cout << "The number of points earned in the FOURS row is " << turnScr << endl << endl << endl;
-            uppScr[3] = turnScr;
-            uppDone[3] = true;
-            break;
-        case 5:
-            cout << "The number of points earned in the FIVES row is " << turnScr << endl << endl << endl;
-            uppScr[4] = turnScr;
-            uppDone[4] = true;
-            break;
-        case 6:
-            cout << "The number of points earned in the SIXES row is " << turnScr << endl << endl << endl;
-            uppScr[5] = turnScr;
-            uppDone[5] = true;
-            break;
-        default:
-            break;
-        }
+        updateScore(uppScr, uppDone, lwrScr, lwrDone, turnScr, numSel);
     }
     // After all the scores have been filled in, compute the score and output a nicely formatted score card. 
     uppSum = 0;
@@ -243,44 +218,104 @@ void getHold(char hold[], int numDice) {
 }
 
 int turnScore(int dice[], int numDice, int numSel) {
+    vector<int> diceVal = getDiceVals(dice, numDice);
     int turnScr = 0;
-    for (int i = 0; i < numDice; i++) {
-        if (dice[i] == numSel) turnScr += numSel;
+    if (numSel >= 1 && numSel <= 6) {
+        turnScr = diceVal[numSel-1] * numSel;
+    } else if (numSel == 7) {
+        if (linearSearch(diceVal, 3) != -1 || linearSearch(diceVal, 4) != -1 || linearSearch(diceVal, 5) != -1) {
+            for (int i = 0; i < numDice; i++) {
+                turnScr += dice[i];
+            }
+        }
+    } else if (numSel == 8) {
+        if (linearSearch(diceVal, 4) != -1 || linearSearch(diceVal, 5) != -1) {
+            for (int i = 0; i < numDice; i++) {
+                turnScr += dice[i];
+            }
+        }
+    } else if (numSel == 9) {
+        if (linearSearch(diceVal, 2) != -1 && linearSearch(diceVal, 3) != -1) {
+            turnScr = 25;
+        }
+    } else if (numSel == 10) {
+        if ((diceVal[0] >= 1 && diceVal[1] >= 1 && diceVal[2] >= 1 && diceVal[3] >= 1) || // 1, 2, 3, 4
+            (diceVal[1] >= 1 && diceVal[2] >= 1 && diceVal[3] >= 1 && diceVal[4] >= 1) || // 2, 3, 4, 5
+            (diceVal[2] >= 1 && diceVal[3] >= 1 && diceVal[4] >= 1 && diceVal[5] >= 1))   // 3, 4, 5, 6
+        {
+            turnScr = 30;
+        }
+    } else if (numSel == 11) {
+        if ((diceVal[0] >= 1 && diceVal[1] >= 1 && diceVal[2] >= 1 && diceVal[3] >= 1 && diceVal[4] >= 1) || // 1, 2, 3, 4, 5
+            (diceVal[1] >= 1 && diceVal[2] >= 1 && diceVal[3] >= 1 && diceVal[4] >= 1 && diceVal[5] >= 1))   // 2, 3, 4, 5, 6
+        {
+            turnScr = 40;
+        }
+    } else if (numSel == 12) {
+        if (linearSearch(diceVal, 5) != -1) {
+            turnScr = 50;
+        }
+    } else if (numSel == 13) {
+        for (int i = 0; i < numDice; i++) {
+            turnScr += dice[i];
+        }
     }
     return turnScr;
 }
 
-bool gameDone(bool uppDone[], int numUppR) {
+vector<int> getDiceVals(int dice[], int numDice) {
+    vector<int> vals = {0, 0, 0, 0, 0, 0};
+
+    for (int i = 0; i < numDice; i++) {
+        vals[dice[i]-1]++;
+    }
+
+    return vals;
+}
+
+int linearSearch(vector<int> vec, int target) {
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (vec[i] == target) {
+            return (int)i; // Return the index if found
+        }
+    }
+    return -1; // Return -1 if not found
+}
+
+bool gameDone(bool uppDone[], int numUppR, bool lwrDone[], int numLwrR) {
     bool done = true;
     for (int i = 0; i < numUppR; i++) {
         if (uppDone[i] == false) done = false;
     }
+    for (int i = 0; i < numLwrR; i++) {
+        if (lwrDone[i] == false) done = false;
+    }
     return done;
 }
 
-void displayScore(int uppScr[], bool uppDone[], int numUppR) {
+void displayScore(int uppScr[], bool uppDone[], int numUppR, int lwrScr[], bool lwrDone[], int numLwrR) {
     cout << "Current Scores:" << endl;
 
     for (int i = 0; i < numUppR; i++) {
         switch (i)
         {
         case 0:
-            cout << "ACES:   ";
+            cout << "Aces:         ";
             break;
         case 1:
-            cout << "TWOS:   ";
+            cout << "Twos:         ";
             break;
         case 2:
-            cout << "THREES: ";
+            cout << "Threes:       ";
             break;
         case 3:
-            cout << "FOURS:  ";
+            cout << "Fours:        ";
             break;
         case 4:
-            cout << "FIVES:  ";
+            cout << "Fives:        ";
             break;
         case 5:
-            cout << "SIXES:  ";
+            cout << "Sixes:        ";
             break;
         default:
             break;
@@ -289,17 +324,63 @@ void displayScore(int uppScr[], bool uppDone[], int numUppR) {
         if (uppDone[i]) cout << setw(2) << uppScr[i];
         cout << endl;
     }
+
+    for (int i = 0; i < numLwrR; i++) {
+        switch (i)
+        {
+        case 0:
+            cout << "3 of a kind:  ";
+            break;
+        case 1:
+            cout << "4 of a kind:  ";
+            break;
+        case 2:
+            cout << "Full House:   ";
+            break;
+        case 3:
+            cout << "Sm. Straight: ";
+            break;
+        case 4:
+            cout << "Lg. Straight: ";
+            break;
+        case 5:
+            cout << "YAHTZEE:      ";
+            break;
+        case 6:
+            cout << "Chance:       ";
+            break;
+        default:
+            break;
+        }
+
+        if (lwrDone[i]) cout << setw(2) << lwrScr[i];
+        cout << endl;
+    }
 }
 
-int selScore(bool uppDone[], int numUppR) {
-    int numSel;
+void selScore(bool uppDone[], bool lwrDone[], int &numSel) {
     do {
-        cout << "Which row would you like to score this turn? (1-6): ";
+        if (uppDone[0] == false) cout << "1. Aces" << endl;
+        if (uppDone[1] == false) cout << "2. Twos" << endl;
+        if (uppDone[2] == false) cout << "3. Threes" << endl;
+        if (uppDone[3] == false) cout << "4. Fours" << endl;
+        if (uppDone[4] == false) cout << "5. Fives" << endl;
+        if (uppDone[5] == false) cout << "6. Sixes" << endl;
+
+        if (lwrDone[0] == false) cout << "7. 3 of a kind" << endl;
+        if (lwrDone[1] == false) cout << "8. 4 of a kind" << endl;
+        if (lwrDone[2] == false) cout << "9. Full House" << endl;
+        if (lwrDone[3] == false) cout << "10. Small Straight" << endl;
+        if (lwrDone[4] == false) cout << "11. Large Straight" << endl;
+        if (lwrDone[5] == false) cout << "12. YAHTZEE" << endl;
+        if (lwrDone[6] == false) cout << "13. Chance" << endl;
+
+        cout << "Which row would you like to score this turn? (1-13): ";
         cin >> numSel; 
-        if (numSel < 1 || numSel > 6) cout << "Selected number out of range" << endl;
-        else if (uppDone[numSel-1]) cout << "Selected number has already been scored" << endl;
-    } while (numSel < 1 || numSel > 6 || uppDone[numSel-1]);
-    return numSel;
+        if (numSel < 1 || numSel > 13) cout << "Selected number out of range" << endl;
+        else if (numSel >= 1 && numSel <= 6 && uppDone[numSel-1]) cout << "Selected number has already been scored" << endl;
+        else if (numSel >= 7 && numSel <= 13 && lwrDone[numSel-7]) cout << "Selected number has already been scored" << endl;
+    } while (numSel < 1 || numSel > 13 || (numSel >= 1 && numSel <= 6 && uppDone[numSel-1]) || (numSel >= 7 && numSel <= 13 && lwrDone[numSel-7]));
 }
 
 void displayTurn() {
@@ -335,4 +416,80 @@ void bubSort(int a[],int n){
         } 
        redList--;
     }while(swap);
+}
+
+void updateScore(int uppScr[], bool uppDone[], int lwrScr[], bool lwrDone[], int turnScr, int numSel) {
+    // Output the number of points earned for the user to see
+    // Save the number of points in the variable for the row
+    // Set the bool of that row to true to indicate that the row has been scored
+    switch (numSel)
+    {
+    case 1:
+        cout << "The number of points earned in the Aces row is " << turnScr << endl << endl << endl;
+        uppScr[0] = turnScr;
+        uppDone[0] = true;
+        break;
+    case 2:
+        cout << "The number of points earned in the Twos row is " << turnScr << endl << endl << endl;
+        uppScr[1] = turnScr;
+        uppDone[1] = true;
+        break;
+    case 3:
+        cout << "The number of points earned in the Threes row is " << turnScr << endl << endl << endl;
+        uppScr[2] = turnScr;
+        uppDone[2] = true;
+        break;
+    case 4:
+        cout << "The number of points earned in the Fours row is " << turnScr << endl << endl << endl;
+        uppScr[3] = turnScr;
+        uppDone[3] = true;
+        break;
+    case 5:
+        cout << "The number of points earned in the Fives row is " << turnScr << endl << endl << endl;
+        uppScr[4] = turnScr;
+        uppDone[4] = true;
+        break;
+    case 6:
+        cout << "The number of points earned in the Sizes row is " << turnScr << endl << endl << endl;
+        uppScr[5] = turnScr;
+        uppDone[5] = true;
+        break;
+    case 7:
+        cout << "The number of points earned in the 3 of a kind row is " << turnScr << endl << endl << endl;
+        lwrScr[0] = turnScr;
+        lwrDone[0] = true;
+        break;
+    case 8:
+        cout << "The number of points earned in the 4 of a kind row is " << turnScr << endl << endl << endl;
+        lwrScr[1] = turnScr;
+        lwrDone[1] = true;
+        break;
+    case 9:
+        cout << "The number of points earned in the Full House row is " << turnScr << endl << endl << endl;
+        lwrScr[2] = turnScr;
+        lwrDone[2] = true;
+        break;
+    case 10:
+        cout << "The number of points earned in the Small Straight row is " << turnScr << endl << endl << endl;
+        lwrScr[3] = turnScr;
+        lwrDone[3] = true;
+        break;
+    case 11:
+        cout << "The number of points earned in the Large Straight row is " << turnScr << endl << endl << endl;
+        lwrScr[4] = turnScr;
+        lwrDone[4] = true;
+        break;
+    case 12:
+        cout << "The number of points earned in the YAHTZEE row is " << turnScr << endl << endl << endl;
+        lwrScr[5] = turnScr;
+        lwrDone[5] = true;
+        break;
+    case 13:
+        cout << "The number of points earned in the Chance row is " << turnScr << endl << endl << endl;
+        lwrScr[6] = turnScr;
+        lwrDone[6] = true;
+        break;
+    default:
+        break;
+    }
 }
