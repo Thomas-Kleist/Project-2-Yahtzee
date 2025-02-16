2fce0f984ccf4fbeae2117bf1f97c7034652d908
Thomas Kleist <thomasfkleist@gmail.com>
2025-02-15 17:00:17 -0800

    Clean up code, add comments

--- yahtzee.cpp
+++ yahtzee.cpp
@@ -23,36 +23,36 @@ using namespace std;
 //Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
 
 // Function Prototypes
-void roll(int [], int);                         // Role dice with no hold
-void roll(int [], char[], int);                 // Role dice with hold
-void dispDce(int [], int);                      // Display dice
-void getHold(char[], int);                      // Ask the player if they would like to hold each dice
-bool gameDone(bool [], int, bool [], int);      // Get if the game is completed based on the row completion
-void displayScore(int [], bool [], int, int [], bool [], int);        // Display the current scores
-void selScore(bool [], bool [], int&);              // Select which row to score
-void displayTurn();                             // Display the turn number
-void selSort(int [],int);                       // Selection sort
-void bubSort(int [],int);                       // Selection sort
-void updateScore(int [], bool [], int[], bool [], int, int); // Update the scores based on a turn score
+void roll(int [], int);                                         // Role dice with no hold
+void roll(int [], char[], int);                                 // Role dice with hold
+void dispDce(int [], int);                                      // Display dice
+void getHold(char[], int);                                      // Ask the player if they would like to hold each dice
+bool gameDone(bool [], int, bool [], int);                      // Get if the game is completed based on the row completion
+void dispScr(int [], bool [], int, int [], bool [], int);       // Display the current scores
+void selRow(bool [], bool [], int&);                            // Select which row to score
+void dispTrn();                                                 // Display the turn number
+void selSort(int [],int);                                       // Selection sort
+void bubSort(int [],int);                                       // Bubble sort
+void updScr(int [], bool [], int[], bool [], int, int);         // Update the scores based on a turn score
 
-int turnScore(int [], int, int);                // Get the score for the turn
-vector<int> getDiceVals(int [], int);           // Get the number of each value of dice
-int linearSearch(vector<int>, int);
+int trnScr(int [], int, int);                                   // Get the score for the turn
+vector<int> getCnt(int [], int);                                // Get the number of each value of dice
+int linSrch(vector<int>, int);                                  // Linear search to find if an int is present in a vector
 
-int totalScore(int [], int[], int, int, int &, int &, int&, int&);
+int ttlScr(int [], int[], int, int, int &, int &, int&, int&);  // Calculate the total score
 
-void avg(int [][2], int [], int [], int, int, int, int, int, float &);
-void dispFin(int [][2], float, int = 7);
+void avg(int [][2], int [], int [], int, int, int, int, int, float &);  // Calculate the averages
+void dispFin(int [][2], float, int = 7);                                // Display the final score and average
 
 int main(int argc, char** argv) {
     // Set Random Number seed
     srand(static_cast<unsigned int>(time(0)));
 
-    const int bnsCut = 63; // The minimum score the player must earn to get the bonus
-    const int bnsScr = 35; // The bonus score the player earns if they meet the cutoff
-    const int numDice = 5;
-    const int numUppR = 6;
-    const int numLwrR = 7;
+    const int bnsCut = 63;  // The minimum score the player must earn to get the bonus
+    const int bnsScr = 35;  // The bonus score the player earns if they meet the cutoff
+    const int numDice = 5;  // The number of dice
+    const int numUppR = 6;  // The number of rows in the upper section
+    const int numLwrR = 7;  // The number of rows in the lower section
 
     //Declare Variables
     int dice[numDice]; // The value of the dice
@@ -66,39 +66,39 @@ int main(int argc, char** argv) {
     int lwrScr[numLwrR]; // The score for each of numbers on a die
     bool lwrDone[numLwrR]; // Bool to store if a certain number has been scored
     
-    int uppSum, uppBns, uppTtl; // The sum of all the scores, the bonus points, and the total score for the game
-    int lwrTtl;
-    int total;
+    int uppSum, uppBns, uppTtl; // The sum of all the scores in the upper section, the bonus points, and the total score for the upper section
+    int lwrTtl;                 // The total score for the lower section
+    int total;                  // The total score for the game
 
-    int scrSht[19][2];
-    float stdDev;
+    int scrSht[19][2];          // The score sheet, used for displaying the final score
+    float stdDev;               // The standard deviation of the final scores
 
     //Initialize Variables
-    for (int i = 0; i < numUppR; i++) {
+    for (int i = 0; i < numUppR; i++) { // Loop through the rows in the upper section and set the scores to 0, the completion to false
         uppScr[i] = 0;
         uppDone[i] = false;
     }
-    for (int i = 0; i < numLwrR; i++) {
+    for (int i = 0; i < numLwrR; i++) { // Loop through the rows in the lower section and set the scores to 0, the completion to false
         lwrScr[i] = 0;
         lwrDone[i] = false;
     }
 
     // Run the game
-    // Run the loop until all numbers have been scored
+    // Run the loop until all rows have been scored
     while (!gameDone(uppDone, numUppR, lwrDone, numLwrR)) {
         // Display turn count and current scores
-        displayTurn();
-        displayScore(uppScr, uppDone, numUppR, lwrScr, lwrDone, numLwrR);
+        dispTrn();
+        dispScr(uppScr, uppDone, numUppR, lwrScr, lwrDone, numLwrR);
 
         // Loop 3 times because each turn in yahtzee allows three roles
         for (int i = 0; i < 3; i++) {
-            cout << "Roll " << i+1 << endl;
+            cout << "Roll " << i+1 << endl; // Display the roll count
             
-            if (i == 0) roll(dice, numDice);
-            else roll(dice, hold, numDice);
+            if (i == 0) roll(dice, numDice); // On the first roll, just roll the dice
+            else roll(dice, hold, numDice); // On the second and third turn, roll the dice with hold
 
-            if (i == 0 || i == 2) selSort(dice, numDice);
-            if (i == 1) bubSort(dice, numDice);
+            if (i == 0 || i == 2) selSort(dice, numDice);   // Use selection sort to sort the dice by value on the first and third roll
+            if (i == 1) bubSort(dice, numDice);             // Use bubble sort on the the second roll
 
             // Display the value of each of the dice
             dispDce(dice, numDice);
@@ -110,18 +110,23 @@ int main(int argc, char** argv) {
             cout << endl;
         }
 
-        // After rolling three times, select which number the user would like to score in
-        // If the number selected is out of range, ask them to input again
-        // If the number they selected has already been scored, ask them to input again
-        selScore(uppDone, lwrDone, numSel);
-        turnScr = turnScore(dice, numDice, numSel);
-        updateScore(uppScr, uppDone, lwrScr, lwrDone, turnScr, numSel);
+        // Select the row to score
+        selRow(uppDone, lwrDone, numSel);
+
+        // Calculate the turn score 
+        turnScr = trnScr(dice, numDice, numSel);
+
+        // Update the game score with the turn score
+        updScr(uppScr, uppDone, lwrScr, lwrDone, turnScr, numSel);
     }
 
-    total = totalScore(uppScr, lwrScr, numUppR, numLwrR, uppSum, uppBns, uppTtl, lwrTtl);
+    // Calculate the total score
+    total = ttlScr(uppScr, lwrScr, numUppR, numLwrR, uppSum, uppBns, uppTtl, lwrTtl);
 
+    // Calculate the average score
     avg(scrSht, uppScr, lwrScr, uppSum, uppBns, uppTtl, lwrTtl, total, stdDev);
 
+    // Display the final result
     dispFin(scrSht, stdDev);
 
     //Exit the Program
@@ -129,55 +134,69 @@ int main(int argc, char** argv) {
 }
 
 
-
+// Loop through all the dice and set them to a random value between 1..6
 void roll(int dice[], int numDice) {
     for (int i = 0; i < numDice; i++) {
         dice[i] = rand() % 6 + 1;
     }
 }
 
+// Loop through all the dice, if they are not being held, set them to a random value between 1..6
 void roll(int dice[], char hold[], int numDice) {
     for (int i = 0; i < numDice; i++) {
         if (!(hold[i] == 'Y' || hold[i] == 'y')) dice[i] = rand() % 6 + 1;
     }
 }
 
+// Loop through all the dice and display their value
 void dispDce(int dice[], int numDice) {
     for (int i = 0; i < numDice; i++) {
         cout << "Dice " << i+1 << ": " << dice[i] << endl;
     }
 }
 
+// Loop through all the dice and ask the player if they would like to hold that dice
 void getHold(char hold[], int numDice) {
     for (int i = 0; i < numDice; i++) {
-        // Ask if the user would like to hold dice 1
         cout << "Would you like to hold dice " << i+1 << "? (Y or N): ";
         cin >> hold[i];
     }
 }
 
-int turnScore(int dice[], int numDice, int numSel) {
-    vector<int> diceVal = getDiceVals(dice, numDice);
+// Calculate the score for this turn based on the row selected and the dice
+int trnScr(int dice[], int numDice, int numSel) {
+    // Get the amount of each number on the dice
+    // So if the dice values were 1, 2, 3, 3, 4; then the vector would look like {1, 1, 2, 1, 0, 0}
+    vector<int> diceVal = getCnt(dice, numDice);
+
+    // Initialize the turn score to 0
     int turnScr = 0;
     if (numSel >= 1 && numSel <= 6) {
+        // If in the upper section, set the turn score to the number of the selected value times the selected value
         turnScr = diceVal[numSel-1] * numSel;
     } else if (numSel == 7) {
-        if (linearSearch(diceVal, 3) != -1 || linearSearch(diceVal, 4) != -1 || linearSearch(diceVal, 5) != -1) {
+        // For three of a kind, see if there are at least 3 of any one value
+        if (linSrch(diceVal, 3) != -1 || linSrch(diceVal, 4) != -1 || linSrch(diceVal, 5) != -1) {
+            // If there is, the turn score is the sum of all the dice
             for (int i = 0; i < numDice; i++) {
                 turnScr += dice[i];
             }
         }
     } else if (numSel == 8) {
-        if (linearSearch(diceVal, 4) != -1 || linearSearch(diceVal, 5) != -1) {
+        // For four of a kind, see if there are at least 4 of any one value
+        if (linSrch(diceVal, 4) != -1 || linSrch(diceVal, 5) != -1) {
+            // If there is, the turn score is the sum of all the dice
             for (int i = 0; i < numDice; i++) {
                 turnScr += dice[i];
             }
         }
     } else if (numSel == 9) {
-        if (linearSearch(diceVal, 2) != -1 && linearSearch(diceVal, 3) != -1) {
+        // For full house, check to see if there is 2 of one value and three of another
+        if (linSrch(diceVal, 2) != -1 && linSrch(diceVal, 3) != -1) {
             turnScr = 25;
         }
     } else if (numSel == 10) {
+        // For small straight, check if there are any of the following three patterns
         if ((diceVal[0] >= 1 && diceVal[1] >= 1 && diceVal[2] >= 1 && diceVal[3] >= 1) || // 1, 2, 3, 4
             (diceVal[1] >= 1 && diceVal[2] >= 1 && diceVal[3] >= 1 && diceVal[4] >= 1) || // 2, 3, 4, 5
             (diceVal[2] >= 1 && diceVal[3] >= 1 && diceVal[4] >= 1 && diceVal[5] >= 1))   // 3, 4, 5, 6
@@ -185,28 +204,34 @@ int turnScore(int dice[], int numDice, int numSel) {
             turnScr = 30;
         }
     } else if (numSel == 11) {
+        // For large straight, check if there are either of the following two patterns
         if ((diceVal[0] >= 1 && diceVal[1] >= 1 && diceVal[2] >= 1 && diceVal[3] >= 1 && diceVal[4] >= 1) || // 1, 2, 3, 4, 5
             (diceVal[1] >= 1 && diceVal[2] >= 1 && diceVal[3] >= 1 && diceVal[4] >= 1 && diceVal[5] >= 1))   // 2, 3, 4, 5, 6
         {
             turnScr = 40;
         }
     } else if (numSel == 12) {
-        if (linearSearch(diceVal, 5) != -1) {
+        // For YAHTZEE, check if there are 5 of any value
+        if (linSrch(diceVal, 5) != -1) {
             turnScr = 50;
         }
     } else if (numSel == 13) {
+        // For chance, the turn score is the sum of all the dice
         for (int i = 0; i < numDice; i++) {
             turnScr += dice[i];
         }
     } else if (numSel == 14) {
+        // If they select to exit, exit the program
         exit(0);
     }
     return turnScr;
 }
 
-vector<int> getDiceVals(int dice[], int numDice) {
+// Return the number of each value on the dice
+vector<int> getCnt(int dice[], int numDice) {
     vector<int> vals = {0, 0, 0, 0, 0, 0};
 
+    // Loop through each dice and count how many of each value there is
     for (int i = 0; i < numDice; i++) {
         vals[dice[i]-1]++;
     }
@@ -214,8 +239,9 @@ vector<int> getDiceVals(int dice[], int numDice) {
     return vals;
 }
 
-int linearSearch(vector<int> vec, int target) {
-    for (size_t i = 0; i < vec.size(); ++i) {
+// Search for a specific value in a vector
+int linSrch(vector<int> vec, int target) {
+    for (size_t i = 0; i < vec.size(); ++i) { 
         if (vec[i] == target) {
             return (int)i; // Return the index if found
         }
@@ -223,6 +249,7 @@ int linearSearch(vector<int> vec, int target) {
     return -1; // Return -1 if not found
 }
 
+// Look through all the rows in the upper and lower section to see if all are completed
 bool gameDone(bool uppDone[], int numUppR, bool lwrDone[], int numLwrR) {
     bool done = true;
     for (int i = 0; i < numUppR; i++) {
@@ -234,7 +261,8 @@ bool gameDone(bool uppDone[], int numUppR, bool lwrDone[], int numLwrR) {
     return done;
 }
 
-void displayScore(int uppScr[], bool uppDone[], int numUppR, int lwrScr[], bool lwrDone[], int numLwrR) {
+// Display the current score
+void dispScr(int uppScr[], bool uppDone[], int numUppR, int lwrScr[], bool lwrDone[], int numLwrR) {
     cout << "Current Scores:" << endl;
 
     for (int i = 0; i < numUppR; i++) {
@@ -299,8 +327,10 @@ void displayScore(int uppScr[], bool uppDone[], int numUppR, int lwrScr[], bool 
     }
 }
 
-void selScore(bool uppDone[], bool lwrDone[], int &numSel) {
+// Ask the play which row they would like to score
+void selRow(bool uppDone[], bool lwrDone[], int &numSel) {
     do {
+        // If an option is available, display it to the player
         if (uppDone[0] == false) cout << "1. Aces" << endl;
         if (uppDone[1] == false) cout << "2. Twos" << endl;
         if (uppDone[2] == false) cout << "3. Threes" << endl;
@@ -318,20 +348,23 @@ void selScore(bool uppDone[], bool lwrDone[], int &numSel) {
 
         cout << "14. Exit" << endl;
 
+        // Get which row the player would like
         cout << "Which row would you like to score this turn? (1-14): ";
         cin >> numSel; 
-        if (numSel < 1 || numSel > 14) cout << "Selected number out of range" << endl;
-        else if (numSel >= 1 && numSel <= 6 && uppDone[numSel-1]) cout << "Selected number has already been scored" << endl;
-        else if (numSel >= 7 && numSel <= 13 && lwrDone[numSel-7]) cout << "Selected number has already been scored" << endl;
-    } while (numSel < 1 || numSel > 14 || (numSel >= 1 && numSel <= 6 && uppDone[numSel-1]) || (numSel >= 7 && numSel <= 13 && lwrDone[numSel-7]));
+        if (numSel < 1 || numSel > 14) cout << "Selected number out of range" << endl; // Check that the value is in range
+        else if (numSel >= 1 && numSel <= 6 && uppDone[numSel-1]) cout << "Selected number has already been scored" << endl; // Check that an upper section row isn't already completed
+        else if (numSel >= 7 && numSel <= 13 && lwrDone[numSel-7]) cout << "Selected number has already been scored" << endl; // Check that a lower section row isn't already completed
+    } while (numSel < 1 || numSel > 14 || (numSel >= 1 && numSel <= 6 && uppDone[numSel-1]) || (numSel >= 7 && numSel <= 13 && lwrDone[numSel-7])); // Loop until valid
 }
 
-void displayTurn() {
+// Display the turn number using a static int
+void dispTrn() {
     static int turn;
     turn++;
     cout << "Turn " << turn << endl;
 }
 
+// Selection sort
 void selSort(int a[],int n) {
     for(int pos=0;pos<n-1;pos++){
         int selIndx=pos;
@@ -344,6 +377,7 @@ void selSort(int a[],int n) {
     }
 }
 
+// Bubble sort
 void bubSort(int a[],int n){
     bool swap;
     int redList=n-1;
@@ -361,7 +395,7 @@ void bubSort(int a[],int n){
     }while(swap);
 }
 
-void updateScore(int uppScr[], bool uppDone[], int lwrScr[], bool lwrDone[], int turnScr, int numSel) {
+void updScr(int uppScr[], bool uppDone[], int lwrScr[], bool lwrDone[], int turnScr, int numSel) {
     // Output the number of points earned for the user to see
     // Save the number of points in the variable for the row
     // Set the bool of that row to true to indicate that the row has been scored
@@ -437,26 +471,33 @@ void updateScore(int uppScr[], bool uppDone[], int lwrScr[], bool lwrDone[], int
     }
 }
 
-int totalScore(int uppScr[], int lwrScr[], int numUppR, int numLwrR, int &uppSum, int &uppBns, int &uppTtl, int &lwrTtl) {
+// Calculate the total score
+int ttlScr(int uppScr[], int lwrScr[], int numUppR, int numLwrR, int &uppSum, int &uppBns, int &uppTtl, int &lwrTtl) {
     const int bnsCut = 35;
 
-    // After all the scores have been filled in, compute the score and output a nicely formatted score card. 
+    // Sum all the upper rows
     uppSum = 0;
     for (int i = 0; i < numUppR; i++) {
         uppSum += uppScr[i];
     }
+    // Calculate the upper bonus
     if (uppSum >= bnsCut) uppBns = 35; else uppBns = 0;
+    // Calculate the upper total
     uppTtl = uppSum + uppBns;
 
+    // Sum the lower rows
     lwrTtl = 0;
     for (int i = 0; i < numLwrR; i++) {
         lwrTtl += lwrScr[i];
     }
 
+    // Calculate the total score
     return uppTtl + lwrTtl;
 }
 
+// Calculate the average score of all games played
 void avg(int scrSht[][2], int uppScr[], int lwrScr[], int uppSum, int uppBns, int uppTtl, int lwrTtl, int total, float &stdDev) {
+    // Initialize the values needed
     const string fileNme = "avgData.dat";
     int count;
     int uppTtls[6];
@@ -540,6 +581,7 @@ void avg(int scrSht[][2], int uppScr[], int lwrScr[], int uppSum, int uppBns, in
     avgOut << M2;
     avgOut.close();
 
+    // Fill in the score sheet that will be used by the display
     scrSht[0][0] = uppScr[0];
     scrSht[0][1] = avgs[0];
 
@@ -598,8 +640,8 @@ void avg(int scrSht[][2], int uppScr[], int lwrScr[], int uppSum, int uppBns, in
     scrSht[18][1] = avgs[17];
 }
 
+// Display the final score for the game and average score
 void dispFin(int scrSht[][2], float stdDev, int width) {
-    // Display the score for the game
     cout << "Your score for this game and the average score of games played:" << endl;
     cout << "UPPER SECTION:          " << setw(width) << "Player" << " | " << setw(width) << "Average" << endl;
     cout << "Aces:                   " << setw(width) << scrSht[0][0] << " | " << setw(width) << scrSht[0][1] << endl;
